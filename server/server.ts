0c1b9ed4352564f1a560c4dd6f46d1ce4e4afd86
root <root@DESKTOP-V69LD39>
2024-01-22 02:15:39 +1300

    dangerous

--- server/server.ts
+++ server/server.ts
@@ -9,19 +9,4 @@ server.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'))
 })
 
-require('dotenv').config()
-
-// Discord.js versions ^13.0 require us to explicitly define client intents
-const { Client, Intents } = require('discord.js')
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-})
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-// Log In our bot
-client.login(process.env.CLIENT_TOKEN)
-
 export default server
